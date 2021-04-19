f146105aa183ccb7751310c02cd870a888772054
Mateus Santos <profissinalmag3@gmail.com>
2021-04-19 00:31:31 -0300

    criação da class POST, modificação no admin.py, que o POST tá sendo adicionado lá, e um migrate para atualizar o banco

--- gaming_platform/general_post/admin.py
+++ gaming_platform/general_post/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
+# Register yo
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author')
+#admin.site.register(Post)
+
