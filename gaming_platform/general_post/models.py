7670a689abd3381a32727d38983db00667230867
Douglas Magalhães <douglas_edbr@hotmail.com>
2021-04-19 01:42:49 -0300

    modificação e outros incrementos da classe Post, alteração no setting.py, adicionando um diretórtio media e general_post

--- gaming_platform/general_post/models.py
+++ gaming_platform/general_post/models.py
@@ -5,8 +5,27 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
 
+    STATUS = (
+        ('disponível', 'Disponível'),
+        ('indisponível', 'Indisponível')
+
+    )
+
     title = models.CharField(max_length=200, verbose_name='Título do jogo')
     slug = models.SlugField(max_length=200)
     author = models.ForeignKey(User, verbose_name='Autor', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'general_post', blank=True, null=True, verbose_name='Imagem')
 
+    status = models.CharField(max_length=15, choices=STATUS, default='disponível')
 
+    @property
+    def views_image(self):
+        return mark_safe('<img scr="%s" width="400px" />' % self.imagem.url)
+       # view_image.short_description = "Imagem Cadastrada"
+       # view_image.allow_tags = True
+
+    #class Meta:
+     #   ordering = ('-disponível',)
+
+    def __str__(self):
+        return str(self.title).capitalize()
