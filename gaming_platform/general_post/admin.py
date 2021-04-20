4bd2cc0d8167b0f7f90da2ee382b9c1f5f886197
Mateus Santos <coringamag3@gmail.com>
2021-04-19 22:38:12 -0300

    adicionando mais conteúdo para a classe POST, e modificando o POSTADMIN do admin.py

--- gaming_platform/general_post/admin.py
+++ gaming_platform/general_post/admin.py
@@ -4,6 +4,16 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author')
+    list_display = ('title', 'author', 'status', 'value')
+    list_filter = ('status', 'value', 'author')
+    #readonly_fields = ('visualizar_imagem',)
+    #date_hierarchy = 'disponivel'
+    raw_id_fields = ('author',)
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+    '''def visualizar_imagem(self, obj):
+        return obj.view_image
+    visualizar_imagem.short_description = "Imagem Cadastrada"'''
 #admin.site.register(Post)
 
--- gaming_platform/general_post/models.py
+++ gaming_platform/general_post/models.py
@@ -6,26 +6,21 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 
     STATUS = (
-        ('disponível', 'Disponível'),
-        ('indisponível', 'Indisponível')
+        ('disponivel', 'Disponivel'),
+        ('indisponivel', 'Indisponivel')
 
     )
 
-    title = models.CharField(max_length=200, verbose_name='Título do jogo')
-    slug = models.SlugField(max_length=200)
-    author = models.ForeignKey(User, verbose_name='Autor', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to = 'general_post', blank=True, null=True, verbose_name='Imagem')
+    title = models.CharField(max_length=200, verbose_name='Título do jogo',  null=True)
+    slug = models.SlugField(max_length=200, null=True)
+    author = models.ForeignKey(User, verbose_name='Autor', null=True, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'general_post', null=True, verbose_name='Imagem')
+    description = models.TextField(max_length=500, verbose_name='Descrição', null=True)
+    status = models.CharField(max_length=15, choices=STATUS, default='disponível',  null=True)
+    value = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Valor', null=True,)
 
-    status = models.CharField(max_length=15, choices=STATUS, default='disponível')
-
-    @property
-    def views_image(self):
-        return mark_safe('<img scr="%s" width="400px" />' % self.imagem.url)
-       # view_image.short_description = "Imagem Cadastrada"
-       # view_image.allow_tags = True
-
-    #class Meta:
-     #   ordering = ('-disponível',)
+    '''class Meta:
+        ordering = ('-disponivel',)'''
 
     def __str__(self):
         return str(self.title).capitalize()
