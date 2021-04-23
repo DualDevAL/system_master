a62e63d4257945ad76c88e31ae1bda06544c6ad2
Mateus Santos <coringamag3@gmail.com>
2021-04-22 21:47:57 -0300

    Criação de uma nova class que dará a função de de adicionar gêneros, modificando o django admin, modificações no .gitignore e README, e por fim criando os requirements da aplicação.

--- gaming_platform/general_post/admin.py
+++ gaming_platform/general_post/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, GameCategory
 # Register yo
 
+
+@admin.register(GameCategory)# usa esse se não funcionar as alteração
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'servant', 'published')
+    list_filter = ('name', 'servant', 'published')
+    date_hierarchy = 'published'
+    search_fields = ('name',)
+
+
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostgAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'status', 'value')
     list_filter = ('status', 'value', 'author')
     #readonly_fields = ('visualizar_imagem',)
@@ -15,5 +24,8 @@ class PostAdmin(admin.ModelAdmin):
     '''def visualizar_imagem(self, obj):
         return obj.view_image
     visualizar_imagem.short_description = "Imagem Cadastrada"'''
-#admin.site.register(Post)
+
+#admin.site.register(GameCategory)
+
+
 
