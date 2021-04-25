1ed92899fc7b8ad654a0618a90b93ffd058f44eb
Douglas Magalhães <douglas_edbr@hotmail.com>
2021-04-25 19:11:52 -0300

    Finalização do projeto 1/2, detalhes: adicionando as classes para requisitos de sistema operacional e idiomas no models, incrementando as mesmas classses no admin.

--- gaming_platform/general_post/admin.py
+++ gaming_platform/general_post/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
-from .models import Post, GameCategory
+from .models import Post, GameCategory, publisher, Select_Language, Operational_System
 # Register yo
 
 
-@admin.register(GameCategory)# usa esse se não funcionar as alteração
+@admin.register(GameCategory)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'servant', 'published')
+    list_filter = ('name', 'servant', 'published')
+    date_hierarchy = 'published'
+    search_fields = ('name',)
+
+
+@admin.register(Select_Language)
+class Select_Language(admin.ModelAdmin):
+    list_display = ('name', 'servant', 'published')
+    list_filter = ('name', 'servant', 'published')
+    date_hierarchy = 'published'
+    search_fields = ('name',)
+
+
+@admin.register(Operational_System)
+class Operational_System(admin.ModelAdmin):
+    list_display = ('name', 'servant', 'published')
+    list_filter = ('name', 'servant', 'published')
+    date_hierarchy = 'published'
+    search_fields = ('name',)
+
+
+@admin.register(publisher)
+class publisherAdmin(admin.ModelAdmin):
     list_display = ('name', 'servant', 'published')
     list_filter = ('name', 'servant', 'published')
     date_hierarchy = 'published'
@@ -14,16 +38,11 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostgAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'status', 'value')
-    list_filter = ('status', 'value', 'author')
-    #readonly_fields = ('visualizar_imagem',)
-    #date_hierarchy = 'disponivel'
+    list_filter = ('publisher', 'value', 'author')
     raw_id_fields = ('author',)
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
 
-    '''def visualizar_imagem(self, obj):
-        return obj.view_image
-    visualizar_imagem.short_description = "Imagem Cadastrada"'''
 
 #admin.site.register(GameCategory)
 
