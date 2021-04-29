48576af5ed83d178221a6b1fb13946697a8a10f3
Mateus Santos <coringamag3@gmail.com>
2021-04-28 23:04:53 -0300

    criação do template login, uma nova aplicação accounts foi criada nela foi modificado a views, urls, forms e a criação do template signup :neckbeard:

--- gaming_platform/gaming_platform/urls.py
+++ gaming_platform/gaming_platform/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static # new
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('general_post.urls')),
 ]
 
--- gaming_platform/general_post/views.py
+++ gaming_platform/general_post/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from . models import Post
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def hello(request):
+    return HttpResponse('olá mundo')
 
 class PostListView(ListView):
     model = Post
