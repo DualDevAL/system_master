cb7823efb6ed838b9e95bcd44b5aae696d0ae474
Mateus Santos <coringamag3@gmail.com>
2021-04-29 23:59:18 -0300

    Delete da aplicação accounts, novas importações e modifações nos templates login, signup :GIFT_HEART:

--- gaming_platform/gaming_platform/urls.py
+++ gaming_platform/gaming_platform/urls.py
@@ -23,8 +23,9 @@ from django.conf.urls.static import static # new
 from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls')),
+    #path("accounts/", include("allauth.urls")),
+    #path('accounts/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('accounts.urls')),
     path('', include('general_post.urls')),
 ]
 
